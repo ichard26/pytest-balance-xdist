9266da92e94c8eda0017f3263f4280c233de6222
Richard Si <sichard26@gmail.com>
2023-05-12 22:42:14 -0400

    Overhaul plugin

--- src/pytest_balance_xdist.py
+++ src/pytest_balance_xdist.py
@@ -1,18 +1,27 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/ichard26/pytest-balance-xdist/blob/main/NOTICE.txt
+#
+# History of changes:
+#
+# - Add (some) type annotations
+# - Add autodiscovery of worker count
+# - Switch to pytest's cache
+# - Make show_worker_times() easier to use (it has a CLI now!)
 
+import argparse
 import collections
 import csv
 import os
 import shutil
 import time
 from pathlib import Path
+from typing import DefaultDict, Generator, Optional, Union
 
 import pytest
 import xdist.scheduler
 
 __author__ = "Richard Si & Ned Batchelder"
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 
 def pytest_addoption(parser):
@@ -33,52 +42,56 @@ def pytest_configure(config):
 class BalanceXdistPlugin:
     """The plugin."""
 
-    def __init__(self, config):
+    def __init__(self, config: pytest.Config) -> None:
         self.config = config
         self.running_all = self.config.getoption("-k") == ""
-        self.times = collections.defaultdict(float)
+        self.times: DefaultDict[str, float] = collections.defaultdict(float)
+        self.chunk_count = self.config.getoption("-n", 4)
         self.worker = os.environ.get("PYTEST_XDIST_WORKER", "none")
-        self.tests_csv = None
+        self.timings_csv: Optional[Path] = None
 
-    def pytest_sessionstart(self, session):
+    def pytest_sessionstart(self, session: pytest.Session) -> None:
         """Called once before any tests are run, but in every worker."""
         if not self.running_all:
             return
 
-        tests_csv_dir = session.startpath.resolve() / "tmp/tests_csv"
-        self.tests_csv = tests_csv_dir / f"{self.worker}.csv"
+        assert session.config.cache is not None, "no cache?!"
+        cache_dir = Path(session.config.cache.mkdir("balance-xdist"))
+        self.timings_csv = Path(cache_dir, f"{self.worker}-timings.csv")
 
-        if self.worker == "none" and tests_csv_dir.exists():
-            for csv_file in tests_csv_dir.iterdir():
+        if self.worker == "none" and cache_dir.exists():
+            for csv_file in cache_dir.iterdir():
                 with csv_file.open(newline="") as fcsv:
                     reader = csv.reader(fcsv)
                     for row in reader:
                         self.times[row[1]] += float(row[3])
-            shutil.rmtree(tests_csv_dir)
+            shutil.rmtree(cache_dir)
 
-    def write_duration_row(self, item, phase, duration):
+    def write_duration_row(self, item: pytest.Item, phase: str, duration: float) -> None:
         """Helper to write a row to the tracked-test csv file."""
-        if self.running_all:
-            self.tests_csv.parent.mkdir(parents=True, exist_ok=True)
-            with self.tests_csv.open("a", newline="") as fcsv:
-                csv.writer(fcsv).writerow([self.worker, item.nodeid, phase, duration])
+        if not self.running_all:
+            return
+
+        assert self.timings_csv is not None
+        with self.timings_csv.open("a", newline="") as fcsv:
+            csv.writer(fcsv).writerow([self.worker, item.nodeid, phase, duration])
 
     @pytest.hookimpl(hookwrapper=True)
-    def pytest_runtest_setup(self, item):
+    def pytest_runtest_setup(self, item: pytest.Item) -> Generator[None, None, None]:
         """Run once for each test."""
         start = time.time()
         yield
         self.write_duration_row(item, "setup", time.time() - start)
 
     @pytest.hookimpl(hookwrapper=True)
-    def pytest_runtest_call(self, item):
+    def pytest_runtest_call(self, item: pytest.Item) -> Generator[None, None, None]:
         """Run once for each test."""
         start = time.time()
         yield
         self.write_duration_row(item, "call", time.time() - start)
 
     @pytest.hookimpl(hookwrapper=True)
-    def pytest_runtest_teardown(self, item):
+    def pytest_runtest_teardown(self, item: pytest.Item) -> Generator[None, None, None]:
         """Run once for each test."""
         start = time.time()
         yield
@@ -88,16 +101,15 @@ class BalanceXdistPlugin:
     def pytest_xdist_make_scheduler(self, config, log):
         """Create our BalancedScheduler using time data from the last run."""
         # Assign tests to chunks
-        nchunks = 8
-        totals = [0] * nchunks
+        totals = [0] * self.chunk_count
         tests = collections.defaultdict(set)
 
-        # first put the difficult ones all in one worker
+        # First put the difficult ones all in one worker
         clumped = set()
         clumps = config.getini("balanced_clumps")
         for i, clump_word in enumerate(clumps):
             clump_nodes = {nodeid for nodeid in self.times.keys() if clump_word in nodeid}
-            i %= nchunks
+            i %= self.chunk_count
             tests[i].update(clump_nodes)
             totals[i] += sum(self.times[nodeid] for nodeid in clump_nodes)
             clumped.update(clump_nodes)
@@ -121,7 +133,7 @@ class BalanceXdistPlugin:
 class BalancedScheduler(xdist.scheduler.LoadScopeScheduling):
     """A balanced-chunk test scheduler for pytest-xdist."""
 
-    def __init__(self, config, log, clumps, test_chunks):
+    def __init__(self, config, log, clumps, test_chunks) -> None:
         super().__init__(config, log)
         self.clumps = clumps
         self.test_chunks = test_chunks
@@ -142,15 +154,12 @@ class BalancedScheduler(xdist.scheduler.LoadScopeScheduling):
         return nodeid
 
 
-# Run this with:
-#   python -c "from tests.balance_xdist_plugin import show_worker_times as f; f()"
-def show_worker_times():
+def show_worker_times(tests_csv_dir: Union[str, os.PathLike]) -> None:
     """Ad-hoc utility to show data from the last tracked-test run."""
-    times = collections.defaultdict(float)
-    tests = collections.defaultdict(int)
-    tests_csv_dir = Path("tmp/tests_csv")
+    times: DefaultDict[str, float] = collections.defaultdict(float)
+    tests: DefaultDict[str, int] = collections.defaultdict(int)
 
-    for csv_file in tests_csv_dir.iterdir():
+    for csv_file in Path(tests_csv_dir).iterdir():
         with csv_file.open(newline="") as fcsv:
             reader = csv.reader(fcsv)
             for row in reader:
@@ -169,3 +178,20 @@ def show_worker_times():
     lo = min(times.values())
     hi = max(times.values())
     print(f"lo = {lo:.2f}; hi = {hi:.2f}; gap = {hi - lo:.2f}; long delta = {hi - avg:.2f}")
+
+
+def cli_entrypoint() -> None:
+    parser = argparse.ArgumentParser(
+        prog="(balance-xdist) show-worker-times",
+        description="Show timing data from last tracked test run.",
+    )
+    parser.add_argument(
+        "--plugin-data-dir",
+        default=Path.cwd() / ".pytest_cache" / "d" / "balance-xdist",
+        type=Path,
+        metavar="PATH",
+        help="Plugin cache directory.",
+    )
+    args = parser.parse_args()
+    print(f"[balance-xdist] Reading data from {args.plugin_data_dir}")
+    show_worker_times(args.plugin_data_dir)
